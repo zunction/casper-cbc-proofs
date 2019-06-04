d5762952313786d72d9b5165f760b6567acaabd4
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2019-06-04 11:40:43 +0300

    Added statistics script

---
+++ statistics.sh
@@ -0,0 +1,11 @@
+echo "Model Variables: parameters and assumptions on them"
+grep "Axiom\|Parameter" -r * --include *.v | wc -l
+echo -e "\nDefinitions: functions, relations, abbreviations"
+grep "Inductive\|Definition\|Fixpoint\|Notation" -r * --include *.v | wc -l
+echo -e "\nResults: theorems, lemmas, propositions, corollaries"
+grep "Theorem\|Lemma\|Proposition\|Corollary" -r * --include *.v | wc -l
+echo -en "\nTOTAL\nLINES: "
+find . -name '*.v' | xargs cat | wc -l
+echo -n "BYTES: "
+find . -name '*.v' | xargs cat | wc -c
+
