8c159ee3c42422c0dd9a12ba50d9f4f9752a1409
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2019-06-05 17:07:10 +0300

    Progress on non-triviality result for light model
    Co-authored-by: Denisa Diaconescu <denisa.diaconescu@gmail.com>
    Co-authored-by: Traian Serbanuta <traian.serbanuta@gmail.com>

--- theories/LightStates/threshold.v
+++ theories/LightStates/threshold.v
@@ -1,6 +1,9 @@
-Require Import List.
-
 Require Import Coq.Reals.Reals.
+Require Import List.
+Require Import Coq.Lists.ListSet.
+
+Require Import Casper.LightStates.validators.
+Require Import Casper.LightStates.fault_weights.
 
 (************************************************************)
 (** Fault tolerance threshold (a non-negative real number) **)
@@ -9,3 +12,21 @@ Require Import Coq.Reals.Reals.
 Parameter t : R.
 
 Parameter threshold_nonnegative : (t >= 0)%R .
+
+(**
+  NOTE: Because lists are finite (by definition), the assumption below
+  states that there exists a finite set of validators whose weight is 
+  larger than the threshold.
+**)
+
+Parameter byzantine_fault_tolerance :
+  exists (vs : list V), NoDup vs /\ (sum_weights vs > t)%R.
+
+Lemma byzantine_fault_tolerance_interval :
+  exists (vs : list V),
+    NoDup vs /\
+    (sum_weights vs > t)%R /\
+    exists v,
+      In v vs /\
+      (sum_weights (set_remove v_eq_dec v vs) <= t)%R.
+  Admitted.
