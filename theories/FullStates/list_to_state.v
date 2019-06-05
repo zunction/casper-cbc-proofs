76cc3eed99bdabd10f6accdeb50cd0cfb1c35a22
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2019-06-06 00:33:28 +0300

    added missing files

---
+++ theories/FullStates/list_to_state.v
@@ -0,0 +1,42 @@
+Require Import List.
+
+Require Import Casper.ListSetExtras.
+
+Require Import Casper.FullStates.messages.
+Require Import Casper.FullStates.states.
+Require Import Casper.FullStates.add_in_sorted.
+Require Import Casper.FullStates.locally_sorted.
+
+Definition list_to_state (msgs : list message) : state :=
+  fold_right add_in_sorted_fn Empty msgs.
+
+Lemma list_to_state_locally_sorted : forall msgs,
+  Forall locally_sorted_msg msgs ->
+  locally_sorted (list_to_state msgs).
+Proof.
+  induction msgs; simpl; try constructor; intros.
+  apply add_in_sorted_sorted with a (list_to_state msgs).
+  - apply IHmsgs. apply Forall_forall. intros msg Hin.
+    rewrite Forall_forall in H. apply H. right. assumption.
+  - apply Forall_inv with msgs. assumption.
+  - apply add_in_sorted_function. reflexivity.
+Qed.
+
+Lemma list_to_state_iff : forall msgs : list message,
+  set_eq (get_messages (list_to_state msgs)) msgs.
+Proof.
+  induction msgs; intros.
+  - simpl. split; apply incl_refl.
+  - simpl. apply set_eq_tran with (a :: (get_messages (list_to_state msgs))).
+    + apply set_eq_add_in_sorted.
+    + apply set_eq_cons. assumption.
+Qed.
+
+Lemma list_to_state_sorted : forall sigma,
+  locally_sorted sigma ->
+  list_to_state (get_messages sigma) = sigma.
+Proof.
+  intros. induction H; try reflexivity.
+  rewrite get_messages_next. simpl. rewrite IHlocally_sorted2.
+  rewrite add_in_sorted_next. rewrite H0. reflexivity.
+Qed.
---
+++ theories/FullStates/state_union.v
@@ -0,0 +1,103 @@
+Require Import List.
+Require Import Coq.Lists.ListSet.
+
+Require Import Casper.ListSetExtras.
+
+Require Import Casper.FullStates.messages.
+Require Import Casper.FullStates.states.
+Require Import Casper.FullStates.add_in_sorted.
+Require Import Casper.FullStates.locally_sorted.
+Require Import Casper.FullStates.syntactic_state_inclusion.
+Require Import Casper.FullStates.list_to_state.
+
+Definition state_union (sigma1 sigma2 : state) : state :=
+  (list_to_state (messages_union (get_messages sigma1) (get_messages sigma2))).
+
+Lemma state_union_messages : forall sigma1 sigma2,
+  set_eq (get_messages (state_union sigma1 sigma2)) (messages_union (get_messages sigma1) (get_messages sigma2)).
+Proof.
+  intros.
+  apply list_to_state_iff.
+Qed.
+
+
+Lemma state_union_incl_right : forall sigma1 sigma2,
+  syntactic_state_inclusion sigma2 (state_union sigma1 sigma2).
+Proof.
+  intros. intros msg Hin. apply state_union_messages. apply set_union_incl_right. assumption.
+Qed.
+
+Lemma state_union_incl_left : forall sigma1 sigma2,
+  syntactic_state_inclusion sigma1 (state_union sigma1 sigma2).
+Proof.
+  intros. intros msg Hin. apply state_union_messages. apply set_union_incl_left. assumption.
+Qed.
+
+Lemma state_union_iff : forall msg sigma1 sigma2,
+  state_in msg (state_union sigma1 sigma2) <-> state_in msg sigma1 \/ state_in msg sigma2.
+Proof.
+  intros; unfold state_union; unfold state_in; split; intros.
+  - apply state_union_messages in H. unfold messages_union in H. rewrite set_union_iff in H. assumption.
+  - apply state_union_messages. unfold messages_union. rewrite set_union_iff. assumption.
+Qed.
+
+Lemma state_union_incl_iterated : forall sigmas sigma,
+  In sigma sigmas ->
+  syntactic_state_inclusion sigma (fold_right state_union Empty sigmas).
+Proof.
+  induction sigmas; intros.
+  - inversion H.
+  - simpl. destruct H.
+    + subst. apply state_union_incl_left.
+    + apply IHsigmas in H. apply incl_tran with (get_messages (fold_right state_union Empty sigmas)); try assumption.
+      apply state_union_incl_right.
+Qed.
+
+Lemma state_union_sorted : forall sigma1 sigma2,
+  locally_sorted sigma1 ->
+  locally_sorted sigma2 ->
+  locally_sorted (state_union sigma1 sigma2).
+Proof.
+  intros.
+  apply locally_sorted_all in H as Hall1. rewrite Forall_forall in Hall1.
+  apply locally_sorted_all in H0 as Hall2. rewrite Forall_forall in Hall2.
+  apply list_to_state_locally_sorted. apply Forall_forall. intros msg Hin.
+  unfold messages_union in Hin.
+  rewrite set_union_iff in Hin. destruct Hin.
+  - apply Hall1. assumption.
+  - apply Hall2. assumption.
+Qed.
+
+Lemma state_union_add_in_sorted : forall sigma1 msg2 sigma2,
+  locally_sorted sigma1 ->
+  locally_sorted sigma2 ->
+  locally_sorted_msg msg2 ->
+  state_union sigma1 (add_in_sorted_fn msg2 sigma2) = add_in_sorted_fn msg2 (state_union sigma1 sigma2).
+Proof.
+  intros.
+  apply sorted_syntactic_state_inclusion_equality_predicate.
+  - apply state_union_sorted; try assumption. 
+    apply add_in_sorted_sorted with msg2 sigma2; try assumption.
+    apply add_in_sorted_function; reflexivity.
+  - apply add_in_sorted_sorted with msg2 (state_union sigma1 sigma2); try assumption.
+    + apply state_union_sorted; assumption.
+    + apply add_in_sorted_function; reflexivity.
+  - intros msg Hin. 
+    apply state_union_iff in Hin.
+    apply set_eq_add_in_sorted.
+    destruct Hin as [Hin | Hin].
+    + right. apply state_union_iff. left; assumption.
+    + apply set_eq_add_in_sorted in Hin. destruct Hin as [Heq | Hin]; subst.
+      * left; reflexivity.
+      * right.  apply state_union_iff. right; assumption.
+  - intros msg Hin.
+    apply set_eq_add_in_sorted in Hin.
+    apply state_union_iff.
+    destruct Hin as [Heq | Hin]; subst.
+    + right. apply set_eq_add_in_sorted. left; reflexivity.
+    + apply state_union_iff in Hin.
+      destruct Hin.
+      * left; assumption.
+      * right. apply set_eq_add_in_sorted. 
+      right; assumption.
+Qed.
