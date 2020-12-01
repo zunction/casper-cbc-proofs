7a89dfd58b45a9990376a27728d0c95e90af254e
bmmoore <brandon.moore@runtimeverification.com>
2020-11-30 21:36:29 -0600

    Make setting changes in SsrExport non-transitive (#148)
    Changes the Global Set command to Export Set.
    The settings in SsrExport has been affecting not just
    files that directly import it, but also any other file
    importing those files.
    Among other changes, this disabled enforcing the bullet
    structure of proof.

--- Lib/SsrExport.v
+++ Lib/SsrExport.v
@@ -1,4 +1,4 @@
 From Coq Require Export ssreflect.
-Global Set SsrOldRewriteGoalsOrder.
-Global Set Asymmetric Patterns.
-Global Set Bullet Behavior "None".
+Export Set SsrOldRewriteGoalsOrder.
+Export Set Asymmetric Patterns.
+Export Set Bullet Behavior "None".
