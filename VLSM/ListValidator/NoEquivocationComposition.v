7aa9ff9185d1b8298c144989b520c7b53afb92b5
mcalancea <mihai.calancea@gmail.com>
2021-02-18 16:14:38 +0200

    Clean up some clutter.

--- VLSM/ListValidator/NoEquivocationComposition.v
+++ VLSM/ListValidator/NoEquivocationComposition.v
@@ -16,7 +16,7 @@ Require Import
   CBC.Equivocation.
 
 Section Composition.
-
+(* 
 Context
   {index : Type}
   {i0 : Inhabited index}
@@ -25,12 +25,11 @@ Context
   {idec : EqDecision index}
   (IM_index := fun (i : index) => @VLSM_list index i index_listing idec ListValidator.estimator)
   {constraint : composite_label IM_index -> (composite_state IM_index) * option (@message index index_listing) -> Prop}
-  (has_been_sent_capabilities := fun i : index => @has_been_sent_lv index i index_listing Hfinite idec ListValidator.estimator)
+  (has_been_sent_capabilities := fun i : index => @lv_sent_capability index i index_listing Hfinite idec ListValidator.estimator)
   (Free := @free_composite_vlsm _ _ idec IM_index _ )
-  (composite_has_been_sent_capability : has_been_sent_capability Free := composite_has_been_sent_capability IM_index (free_constraint IM_index) Hfinite has_been_sent_capabilities)
   (X := composite_vlsm IM_index (no_equivocations Free))
   (preX := pre_loaded_with_all_messages_vlsm X)
   {Mindex : Measurable index}
-  {Rindex : ReachableThreshold index}.
+  {Rindex : ReachableThreshold index}. *)
 
 End Composition.
