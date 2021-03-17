0e682e308730afcaab8d26c3aa906e29a9c67cc0
mcalancea <mihai.calancea@gmail.com>
2021-03-17 15:50:01 +0200

    Add main theorem statement.

--- VLSM/FinalityInspector/Inspector.v
+++ VLSM/FinalityInspector/Inspector.v
@@ -11,26 +11,33 @@ From CasperCBC
 Section FullNodeLike.
 
 Context
+  {CV : consensus_values}
+  {CV_eq_dec : EqDecision C}
   {message : Type}
   {mdec : EqDecision message}
   (happens_before : message -> message -> Prop)
+  (happens_before' := clos_trans _ happens_before)
+  (Hstrict : StrictOrder happens_before)
   (happens_before_dec : RelDecision happens_before)
   (predSet : message -> set message)
   (HpredSetCorrect : forall (m1 m2 : message), happens_before m1 m2 <-> In m2 (predSet m1))
   (downSet : message -> set message)
   (downSet' := fun (m : message) => set_union mdec (downSet m) [m])
-  (HdownSetCorrect : forall (m : message), 
-                       set_eq
-                       (downSet m) 
-                       (fold_right (set_union decide_eq) nil (List.map downSet' (predSet m))))
+  (HdownSetCorrect : forall (m1 m2 : message), happens_before' m1 m2 <-> In m2 (downSet m1))
   {index : Type}
   {index_listing : list index}
+  (n := length index_listing)
   {Hfinite : Listing index_listing}
   {idec : EqDecision index}
   {i0 : Inhabited index}
+  (sender : message -> index)
   {IM : index -> VLSM message}
   (computable_sent : forall (i : index), computable_sent_messages (IM i))
   (computable_received : forall (i : index), computable_received_messages (IM i)). 
+  
+  Definition senders 
+      (s : set message) := 
+      List.map sender s.
   
   Definition sent_set 
     (i : index) 
@@ -66,14 +73,51 @@ Context
     let i := projT1 l in
     let (s', oom) := vtransition (IM i) (projT2 l) ((s i), iom) in
     has_justification_option i (s i) iom /\ has_justification_option i (s i) oom.
-
+    
+   Definition is_equivocating_from_set
+      (sm : set message)
+      (i : index) :=
+      exists (m1 m2 : message), 
+      In m1 sm /\
+      sender m1 = i /\
+      In m2 sm /\ 
+      sender m2 = i /\
+      ~comparable happens_before m1 m2.
+      
+   Definition is_equivocating_from_message
+     (m : message) :=
+     is_equivocating_from_set (downSet m).
+    
+   Local Instance is_equivocating_from_message_dec : RelDecision is_equivocating_from_message.
+   Proof.
+   Admitted.
+    
+   Definition honest_validators_from_message
+     (m : message) : set index := 
+     List.filter (fun (i : index) => negb (bool_decide (is_equivocating_from_message m i))) index_listing.
+    
+   Definition honest_votes_count 
+     (m : message) : nat :=
+     length (honest_validators_from_message m).
+    
+   Definition honest_messages
+     (m : message) : set message :=
+     List.filter (fun (m : message) => inb idec (sender m) (honest_validators_from_message m)) (predSet m).
+      
+   Definition latest_messages
+     (m : message) : set message :=
+     ListSetExtras.get_maximal_elements (fun m1 m2 => bool_decide (happens_before m1 m2)) (honest_messages m).
+    
 Section Inspector.
 
 Context
-    {CV : consensus_values}
-    (sender : message -> index)
-    (latestFrom : message -> index -> message) 
-    (X := composite_vlsm IM fullnode_constraint).
+    (X := composite_vlsm IM fullnode_constraint)
+    (vote : message -> option C)
+    (Hvote : forall (m : message) (c : C),
+             vote m = Some c ->
+             (forall (c' : C),
+             List.count_occ option_eq_dec (List.map vote (latest_messages m)) (Some c) <= 
+             List.count_occ option_eq_dec (List.map vote (latest_messages m)) (Some c'))). 
     
     Definition composite_sent
       (s : vstate X) := 
@@ -93,63 +137,28 @@ Context
       (m : message) :
       In m (composite_observed s) -> incl (downSet m) (composite_observed s).
     Proof.
-    Admitted. 
+    Admitted.
     
-    Definition is_equivocating_from_set
-      (sm : set message)
-      (i : index) :=
-      exists (m1 m2 : message), 
-      In m1 sm /\
-      sender m1 = i /\
-      In m2 sm /\ 
-      sender m2 = i /\
-      ~comparable happens_before m1 m2.
-      
-    Definition is_equivocating_from_message
-      (m : message) :=
-      is_equivocating_from_set (predSet m).
-      
     Definition is_equivocating_from_state
       (s : vstate X) :=
       is_equivocating_from_set (composite_observed s).
-      
-    Program Definition equivocators_from_state
-      (s : vstate X) :=
-      List.filter (fun i => negb (@bool_decide (is_equivocating_from_state s i) _)) index_listing.
-    Next Obligation.
-    Admitted.
     
-    Lemma is_equivocating_from_message_dec : RelDecision is_equivocating_from_message.
+    Local Instance is_equivocating_from_state_dec : RelDecision is_equivocating_from_state.
     Proof.
     Admitted.
     
-    Definition senders 
-      (s : set message) := 
-      List.map sender s.
-    
-    Definition honest
-      (m : message) : set index := 
-      List.filter (fun (i : index) => negb (@bool_decide (is_equivocating_from_message m i) 
-                                              (is_equivocating_from_message_dec m i))) index_listing.
-    
-    Definition honest_messages
-      (m : message) : set message :=
-      List.filter (fun (m : message) => inb idec (sender m) (honest m)) (predSet m).
-      
-    Definition latest_messages
-      (m : message) : set message :=
-      ListSetExtras.get_maximal_elements (fun m1 m2 => bool_decide (happens_before m1 m2)) (honest_messages m).
-    
-    Definition vote
-      (m : message) : option C := None.
+    Definition equivocators_from_state
+      (s : vstate X) :=
+      List.filter (fun i => negb (bool_decide (is_equivocating_from_state s i))) index_listing.
       
     Record committee_skeleton : Type := {
       com_state : vstate X;
-      com_state_Hpr : protocol_state_prop X com_state;
       value : C;
       com : list (set message);
       q : nat;
-      k : nat;
+      k := length com - 1;
+      get_base := last_error com;
+      get_top := hd_error com;
     }.
     
     Definition unanimity 
@@ -167,10 +176,11 @@ Context
       forall (m1 m2 m3 : message),
       In m1 sm /\ In m3 sm ->
       sender m1 = sender m2 /\ sender m3 = sender m2 ->
-      happens_before m1 m2 /\ happens_before m2 m3 ->
+      happens_before' m1 m2 /\ happens_before' m2 m3 ->
       In m2 sm.
-    
-    Definition live_messages
+      
+    (* Definition of Ci' *)
+    Definition relevant_messages
       (sm1 sm2 : set message) :=
       List.filter (fun m => inb idec (sender m) (senders sm2)) sm1. 
     
@@ -179,7 +189,7 @@ Context
       (q : nat) :=
       forall (m : message),
       In m sm1 ->
-      length (senders (set_inter decide_eq (downSet' m) (live_messages sm1 sm2))) >= q.
+      length (senders (set_inter decide_eq (downSet' m) (relevant_messages sm1 sm2))) >= q.
     
     Inductive valid_com_prop : vstate X -> C -> nat -> list (set message) -> Prop :=
     | valid_com_base 
@@ -199,12 +209,31 @@ Context
         (Hconv : convexity sm1)
         (Hgood : valid_com_prop s value q (sm2 :: l)) : valid_com_prop s value q (sm1 :: (sm2 :: l)).
     
-    Definition commitee : Type := {
+    Definition committee : Type := {
       comskel : committee_skeleton | valid_com_prop (com_state comskel) (value comskel) (q comskel) (com comskel)
     }.
     
-    
-      
+    Theorem main
+      (s : vstate X)
+      (value : C)
+      (Hpr : protocol_state_prop X s)
+      (Com : committee)
+      (skel := proj1_sig Com)
+      (q := (q skel))
+      (k := (k skel))
+      (base top : set message)
+      (Hbase : get_base skel = Some base)
+      (Htop : get_top skel = Some top)
+      (Hstate : com_state skel = s)
+      (m : message)
+      (Hm : In m (composite_observed s))
+      (Htouch : exists (m' : message), In m' (downSet m) /\ In m' top) 
+      (Hdifferent : vote m <> Some value) :
+      length (equivocators_from_state s) >= 
+      (2 * q - n) * (1 - (1 / 2 ^ k)).
+      (* Above formula should use Reals.*)
+    Proof.
+    Admitted.
       
 End Inspector.
 End FullNodeLike.
