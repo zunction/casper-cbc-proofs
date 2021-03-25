81de9bb817d741120b70b87bd232bd930a9748d8
Traian Florin Șerbănuță <traian.serbanuta@gmail.com>
2021-03-25 17:08:59 +0200

    Dependent messsages

---
+++ VLSM/DependentMessages.v
@@ -0,0 +1,110 @@
+From Coq
+Require Import
+  FinFun List ListSet
+  .
+Import ListNotations.
+From CasperCBC
+Require Import
+  Preamble Measurable
+  VLSM.Common VLSM.Composition VLSM.Equivocation
+  .
+
+Section dependent_messages.
+
+Context
+  {message : Type}
+  {validator : Type}
+  {index : Type}
+  (sender : message -> option validator)
+  (A : validator -> index)
+  (IM : index -> VLSM message)
+  (Hbs : forall i, has_been_sent_capability (IM i))
+  (Hbr : forall i, has_been_received_capability (IM i))
+  (Hbo := fun i => has_been_observed_capability_from_sent_received (IM i))
+  {i0 : Inhabited index}
+  {IndEqDec : EqDecision index}
+  (X := free_composite_vlsm IM)
+  .
+
+Class DependentMessages
+  :=
+  { dependent_messages : message -> set message
+  ; initial_message_not_dependent (m : message)
+      : composite_initial_message_prop IM m -> dependent_messages m = []
+  ; no_sender_messages_are_initial (m : message)
+      : can_emit (pre_loaded_with_all_messages_vlsm X) m ->
+        sender m = None -> composite_initial_message_prop IM m
+  ; dependent_message (m : message)
+      : forall
+        (v : validator)
+        (Hmv : sender m = Some v)
+        (s : vstate (IM (A v)))
+        (Hsm : protocol_generated_prop (pre_loaded_with_all_messages_vlsm (IM (A v))) s m)
+        (dm : message),
+        In dm (dependent_messages m) <->
+        @state_received_not_sent _ (IM (A v)) (Hbs (A v)) (Hbr (A v)) s dm
+  }.
+
+
+Definition dependent_messages_local_full_node_condition
+  {Hdm : DependentMessages}
+  (i : index)
+  (s : vstate (IM i))
+  (m : message)
+  : Prop
+  := forall dm, In dm (dependent_messages m) ->
+    @has_been_observed _ (IM i) (Hbo i) s dm.
+
+End dependent_messages.
+
+Arguments dependent_messages { _ _ _ _ _ _ _ _ _ _ _} _ .
+
+
+Section dependent_messages_full_node.
+
+Context
+  {message : Type}
+  {validator : Type}
+  {index : Type}
+  (sender : message -> option validator)
+  (A : validator -> index)
+  (IM : index -> VLSM message)
+  (Hbs : forall i, has_been_sent_capability (IM i))
+  (Hbr : forall i, has_been_received_capability (IM i))
+  (Hbo := fun i => has_been_observed_capability_from_sent_received (IM i))
+  {i0 : Inhabited index}
+  {IndEqDec : EqDecision index}
+  (X := free_composite_vlsm IM)
+  {Hdm : DependentMessages sender A IM Hbs Hbr}
+  {index_listing : list index}
+  (finite_index : Listing index_listing)
+  (X_has_been_sent_capability : has_been_sent_capability X := composite_has_been_sent_capability IM (free_constraint IM) finite_index Hbs)
+  (X_has_been_received_capability : has_been_received_capability X := composite_has_been_received_capability IM (free_constraint IM) finite_index Hbr)
+  (X_has_been_observed_capability : has_been_observed_capability X := has_been_observed_capability_from_sent_received X)
+  .
+
+Existing Instance X_has_been_observed_capability.
+
+Definition dependent_messages_global_full_node_condition
+  (s : composite_state IM)
+  (m : message)
+  : Prop
+  :=
+  forall dm, In dm (dependent_messages m) ->
+    has_been_observed X s dm.
+
+Definition dependent_messages_full_node_constraint
+  (l : composite_label IM)
+  (som : composite_state IM * option message)
+  : Prop
+  :=
+  let (s, om) := som in
+  match om with
+  | None => True
+  | Some m => dependent_messages_global_full_node_condition s m
+  end.
+
+End dependent_messages_full_node.
+
+Arguments dependent_messages_global_full_node_condition  { _ _ _ _ _ _ _ _ _ _ _ _ } _ _.
+Arguments dependent_messages_full_node_constraint  { _ _ _ _ _ _ _ _ _ _ _ _} _ _.
--- VLSM/Equivocators/Composition/LimitedEquivocation/LimitedEquivocation.v
+++ VLSM/Equivocators/Composition/LimitedEquivocation/LimitedEquivocation.v
@@ -7,17 +7,18 @@ Require Import
 Import ListNotations.
 From CasperCBC
   Require Import
-    Preamble ListExtras FinExtras
+    Preamble ListExtras ListSetExtras FinExtras
     Lib.Measurable
     VLSM.Common VLSM.Composition VLSM.Equivocation
     VLSM.Equivocators.Common VLSM.Equivocators.Projections
     VLSM.Equivocators.MessageProperties
     VLSM.Equivocators.Composition.Common
+    VLSM.DependentMessages
     .
 
 (** * VLSM Limited Equivocation *)
 
-Section equivocators_composition_projections.
+Section limited_state_equivocation.
 
 Context {message : Type}
   (index := Type)
@@ -52,4 +53,67 @@ Definition equivocators_limited_equivocations_vlsm
   :=
   composite_vlsm equivocator_IM equivocators_limited_equivocations_constraint.
 
-End equivocators_composition_projections.
+End limited_state_equivocation.
+
+Section limited_message_equivocation.
+
+Context
+  {message : Type}
+  {index : Type}
+  {IndEqDec : EqDecision index}
+  (index_listing : list index)
+  (finite_index : Listing index_listing)
+  {validator : Type}
+  {ValMeasurable : Measurable validator}
+  {ValEqDec : EqDecision validator}
+  (IM : index -> VLSM message)
+  (Hbs : forall i, has_been_sent_capability (IM i))
+  (Hbr : forall i, has_been_received_capability (IM i))
+  (Hbo := fun i => has_been_observed_capability_from_sent_received (IM i))
+  (i0 : Inhabited index)
+  (X := free_composite_vlsm IM)
+  (X_has_been_sent_capability : has_been_sent_capability X := composite_has_been_sent_capability IM (free_constraint IM) finite_index Hbs)
+  (X_has_been_received_capability : has_been_received_capability X := composite_has_been_received_capability IM (free_constraint IM) finite_index Hbr)
+  (X_has_been_observed_capability : has_been_observed_capability X := has_been_observed_capability_from_sent_received X)
+  (A : validator -> index)
+  (sender : message -> option validator)
+  (globally_known_equivocators : composite_state IM -> set validator)
+  {Hdm : DependentMessages sender A IM Hbs Hbr}
+  {reachable_threshold : ReachableThreshold validator}
+  .
+
+Existing Instance X_has_been_observed_capability.
+
+Definition known_equivocators_transition_no_equiv_prop : Prop :=
+  forall
+    l s iom s' oom,
+    composite_transition IM l (s, iom) = (s', oom) ->
+    no_additional_equivocations_constraint X l (s, iom) ->
+    set_eq (globally_known_equivocators s') (globally_known_equivocators s).
+
+Definition known_equivocators_transition_equiv_prop : Prop :=
+  forall
+    l s im s' oom v,
+    composite_transition IM l (s, Some im) = (s', oom) ->
+    ~ no_additional_equivocations_constraint X l (s, Some im) ->
+    dependent_messages_global_full_node_condition finite_index s im ->
+    sender im = Some v ->
+    set_eq
+      (globally_known_equivocators s')
+      (set_add decide_eq v (globally_known_equivocators s)).
+
+Definition globally_known_equivocators_weight
+  (s : composite_state IM)
+  : R
+  :=
+  sum_weights (globally_known_equivocators s).
+
+Definition full_node_limited_equivocation_constraint
+  (l : composite_label IM)
+  (som : composite_state IM * option message)
+  :=
+  dependent_messages_full_node_constraint finite_index l som /\
+  let (s', om') := composite_transition IM l som in
+  (globally_known_equivocators_weight s' < proj1_sig threshold)%R.
+
+End limited_message_equivocation.
